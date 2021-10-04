660155f90dd62532e8ec44ca6f18d7268b4d661a
GuilhermeVenturim <guilhermeventurim@outlook.com>
2021-10-04 11:50:17 -0300

    Primeira parte da aula de introdução

---
+++ js/main.js
@@ -0,0 +1,137 @@
+
+
+
+
+/*
+var d = new Date();
+alert(d.getHours()+1);
+alert(d.getDay()+1);
+alert(d.getMonth()+1);
+alert(d.getFullYear()+1);
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+/*
+var frutas = [{nome: "maçã", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].cor);
+*/
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" * "))
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista[1]);
+//alert(lista[1]);
+//console.log(lista.reverse());
+
+/*
+var nome = "Guilherme Venturim";
+var n1 = 5;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão", "Brasil"));
+*/
+
+//OLD
+
+/*
+var d = new Date();
+alert(d);
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 5;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+/*
+var frutas = [{nome: "maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = {nome: "maçã", cor:"vermelha"}
+console.log(fruta);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(listajoin(" = "));
+
+//var nome = "Guilherme Venturim";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Meu javascript";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.replace("Meu", "Nosso"));
+//alert(frase.replace("Meu", "Nosso"))
